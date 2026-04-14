c3a9d0fe81d16a12ab90dd59799ee71db3e6cbc3
Mateus Neves <prmateus.ofn@gmail.com>
2026-04-14 14:07:17 -0300

    Update pcp_1sem_cp2_ex03.py

--- pcp_1sem_cp2_ex03.py
+++ pcp_1sem_cp2_ex03.py
@@ -1,33 +1,42 @@
 #Questao 3, sistema de controle de notas dos alunos
 print(f'==========SISTEMA DE NOTAS==========')
-nome = input('Digite o nome do aluno: ')
-rm = int(input('Digite o RM do aluno: '))
-nota_cp1 = float(input('Digite a nota da CP-1: '))
-nota_cp2 = float(input('Digite a nota da CP-2: '))
-nota_cp3 = float(input('Digite a nota da CP-3: '))
-nota_sp1 = float(input('Digite a nota da Sprint 1 (sp1): '))
-nota_sp2 = float(input('Digite a nota da Sprint 2 (sp2): '))
-nota_gs  = float(input('Digite a nota da Global Solution (gs): '))
-print(f'\n')
 
-#variavel que vai receber a menor nota para calcular as medias
-menor = 0
+#loop while para cadastrar varios alunos
+while True:
+    nome = input('Digite o nome do aluno: ')
+    rm = int(input('Digite o RM do aluno: '))
+    nota_cp1 = float(input('Digite a nota da CP-1: '))
+    nota_cp2 = float(input('Digite a nota da CP-2: '))
+    nota_cp3 = float(input('Digite a nota da CP-3: '))
+    nota_sp1 = float(input('Digite a nota da Sprint 1 (sp1): '))
+    nota_sp2 = float(input('Digite a nota da Sprint 2 (sp2): '))
+    nota_gs  = float(input('Digite a nota da Global Solution (gs): '))
+    print(f'\n')
+    
+    #variavel que vai receber a menor nota para calcular as medias
+    menor = 0
+    
+    #condicionais de verificacao para ver qual a menor nota entre os checkpoints
+    if nota_cp1 <= nota_cp2 and nota_cp1 <= nota_cp3:
+        menor = nota_cp1
+    elif nota_cp2 <= nota_cp1 and nota_cp2 <= nota_cp3:
+        menor = nota_cp2
+    else:
+        menor = nota_cp3
+    
+    #variaveis armazenando as notas medias com e sem peso
+    media = ((nota_cp1 + nota_cp2 + nota_cp3 - menor + nota_sp1 + nota_sp2) / 4) * 0.4 + (nota_gs * 0.6)
+    media_com_peso = media * 0.4
+    
+    #exibindo os resultados
+    print(f'==========RESULTADOS==========')
+    print(f'Nome do aluno: {nome}')
+    print(f'RM do aluno: {rm}')
+    print(f'Média do semestre (sem peso): {media:.2f}')
+    print(f'Média do semestre (com peso): {media_com_peso:.2f}')
 
-#condicionais de verificacao para ver qual a menor nota entre os checkpoints
-if nota_cp1 <= nota_cp2 and nota_cp1 <= nota_cp3:
-    menor = nota_cp1
-elif nota_cp2 <= nota_cp1 and nota_cp2 <= nota_cp3:
-    menor = nota_cp2
-else:
-    menor = nota_cp3
-
-#variaveis armazenando as notas medias com e sem peso
-media = ((nota_cp1 + nota_cp2 + nota_cp3 - menor + nota_sp1 + nota_sp2) / 4) * 0.4 + (nota_gs * 0.6)
-media_com_peso = media * 0.4
-
-#exibindo os resultados
-print(f'==========RESULTADOS==========')
-print(f'Nome do aluno: {nome}')
-print(f'RM do aluno: {rm}')
-print(f'Média do semestre (sem peso): {media:.2f}')
-print(f'Média do semestre (com peso): {media_com_peso:.2f}')
+    #verificacao de vai adicionar mais ou nao, continuar o loop
+    continuar = input('Deseja cadastrar outro aluno? (s / n): ').lower()
+    if continuar != 's':
+        print('Sistema encerrado.')
+        break
