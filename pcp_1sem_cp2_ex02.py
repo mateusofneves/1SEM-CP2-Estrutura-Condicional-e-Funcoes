3dafe5ece7215ef622d9dd359442952f7bf181b9
Patohabitant <p.henriquebilac@gmail.com>
2026-04-14 10:21:29 -0300

    Subindo arquivos Python

--- pcp_1sem_cp2_ex02.py
+++ pcp_1sem_cp2_ex02.py
@@ -1,55 +1,26 @@
-n1 = float(input("Digite algum numero "))
+print("=====TIPO DO TRIÂNGULO=====")
+a = int(input("Digite o valor do lado A: "))
+b = int(input("Digite o valor do lado B: "))
+c = int(input("Digite o valor do lado C: "))
 
-n2 = float(input("Digite algum numero "))
+lados = [a, b, c]
+lados.sort(reverse=True)
 
-n3 = float(input("Digite algum numero "))
+A, B, C = lados
 
-if(n1 > n2 and n1 > n3):
-    A = n1
-    if(n2 > n3):
-        B = n2
-        C = n3
-        print(f"O lado A: {n1}. O lado B:{n2}. O lado C: {n3} \n")
+print(f"O lado A: {A}. O lado B: {B}. O lado {C}")
 
-
-
-    elif(n3 > n2):
-        B = n3
-        C = n2
-        print(f"O lado A: {n1}. O lado B:{n3}. O lado C: {n2} \n")
-
-    elif(n3 == n2):
-        B = n3
-        C = n2
-        print(f"O lado A: {n1}. O lado B:{n3}. O lado C: {n2} \n")
-
-
-
-
-elif(n2 > n1 and n2 > n3):
-    A = n2
-    if (n1 > n3):
-        B = n1
-        C = n3
-
-        print(f"O lado A: {n2}. O lado B:{n1}. O lado C: {n3} \n")
-
-    elif (n3 > n1):
-        B = n3
-        C = n1
-
-        print(f"O lado A: {n2}. O lado B:{n3}. O lado C: {n1} \n")
-
-elif(n3 > n1 and n3 > n2):
-    A = n3
-    if (n1 > n2):
-        B = n1
-        C = n2
-
-        print(f"O lado A: {n3}. O lado B:{n1}. O lado C: {n2} \n")
-
-    elif (n2 > n1):
-        B = n2
-        C = n1
-
-        print(f"O lado A: {n3}. O lado B:{n2}. O lado C: {n1} \n")
+# verifica se forma triângulo
+if A >= B + C:
+    print("NÃO FORMA TRIÂNGULO")
+# tipos
+elif A == B == C:
+    print("TRIÂNGULO EQUILATERO")
+elif A == B or B == C or C == A:
+    print("TRIÂNGULO ISOSCELES")
+elif A**2 == B**2 + C**2:
+    print("TRIÂNGULO RETANGULO")
+elif A**2 > B**2 + C**2:
+    print("TRIÂNGULO OBTUSANGULO")
+else:
+    print("TRIÂNGULO ACUTANGULO")
